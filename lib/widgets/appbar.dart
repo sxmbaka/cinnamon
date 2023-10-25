fcf0b67f78ac4298b7d0bf932ef40797abd31d40
sxmbaka <yatharth.singhhh@gmail.com>
2023-10-25 17:45:29 +0530

    Added icon to appbar

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CinnamonAppBar extends StatefulWidget implements PreferredSizeWidget {
   const CinnamonAppBar({super.key});
@@ -11,27 +12,31 @@ class CinnamonAppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _CinnamonAppBarState extends State<CinnamonAppBar> {
+  final appBarColor = Colors.pink;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      
-        title: Row(
-          children: [
-            const Expanded(
-              child: Text(
-                'Cinnamon',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: appBarColor),
+      backgroundColor: appBarColor,
+      bottomOpacity: 0.3,
+      title: Row(
+        children: [
+          const Expanded(
+            child: Text(
+              'Cinnamon',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      );
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
