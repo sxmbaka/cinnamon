35c9524873a7c6757e4a6e0fe7f409978ae8141a
sxmbaka <yatharth.singhhh@gmail.com>
2023-10-25 14:01:17 +0530

    Added prompt page.

---
+++ lib/pages/prompt_lage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PromptPage extends StatefulWidget {
+  const PromptPage({super.key});
+
+  @override
+  State<PromptPage> createState() => _PromptPageState();
+}
+
+class _PromptPageState extends State<PromptPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
